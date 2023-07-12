eab1e116630ca7d145e0f351b550909b7e745366
Kitwana Akil <kakil@me.com>
2023-07-12 09:37:03 -0400

    Completed the WeSplit check splitting application.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,44 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
-    let students = ["Harry", "Herminoe", "Ron"]
+    var currencyFormatter: FloatingPointFormatStyle<Double>.Currency {
+        return FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currency?.identifier ?? "USD")
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        //calculate the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var grandTotal: Double {
+        let totalCost = totalPerPerson * Double(numberOfPeople + 2)
+        return totalCost
+    }
+
     
     var body: some View {
+        
         NavigationView {
-            VStack {
-                Form {
-                    VStack(alignment: .leading) {
-                        Image(systemName: "globe")
-                            .imageScale(.large)
-                            .foregroundColor(.accentColor)
-                        Text("Hello, world!")
-                        
-                        Button("Tap Count: \(tapCount)") {
-                            tapCount += 1
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
                         }
-                        
-                        TextField("Enter your name: ", text: $name)
-                        Text("Your name is: \(name)")
-                        
                     }
-                    .padding()
                 }
-                .navigationTitle("SwiftUI")
                 
-                Form {
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(students, id: \.self) { Text($0) }
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currencyFormatter)
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                Section {
+                    Text(grandTotal, format: currencyFormatter)
+                } header: {
+                    Text("Check Total")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
             }
-            
-            
         }
-        
     }
 }
 
