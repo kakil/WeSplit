7432f86a947de81c5232eac7224262d38d710bd6
Kitwana Akil <kakil@me.com>
2023-07-12 07:22:30 -0400

    First part of project exploring initial features.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    @State private var selectedStudent = "Harry"
+    
+    let students = ["Harry", "Herminoe", "Ron"]
+    
     var body: some View {
-        Form {
+        NavigationView {
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
+                Form {
+                    VStack(alignment: .leading) {
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundColor(.accentColor)
+                        Text("Hello, world!")
+                        
+                        Button("Tap Count: \(tapCount)") {
+                            tapCount += 1
+                        }
+                        
+                        TextField("Enter your name: ", text: $name)
+                        Text("Your name is: \(name)")
+                        
+                    }
+                    .padding()
+                }
+                .navigationTitle("SwiftUI")
+                
+                Form {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) { Text($0) }
+                    }
+                }
             }
-            .padding()
+            
+            
         }
+        
     }
 }
 
